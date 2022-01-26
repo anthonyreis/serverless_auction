9197d89fa4b51d7db5b1df93d8854dffc687f183
anthonyreis <matheus.anthony1@gmail.com>
2022-01-26 07:17:10 -0300

    validacao de input

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,7 +1,9 @@
 import { v4 as uuid } from 'uuid';
 import AWS from 'aws-sdk';
-import commonMiddleware from '../lib/commonMiddleware';
 import createError from 'http-errors';
+import validator from '@middy/validator';
+import commonMiddleware from '../lib/commonMiddleware';
+import createAuctionSchema from '../lib/schemas/createAuctionSchema';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
@@ -39,4 +41,7 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = commonMiddleware(createAuction);
+export const handler = commonMiddleware(createAuction)
+  .use(validator({
+    inputSchema: createAuctionSchema
+  }))
