eb9359e0d5c8ae99945651e0a61a69d678a5d91a
anthonyreis <matheus.anthony1@gmail.com>
2022-01-26 06:40:04 -0300

    adicionando consulta de auction para fechar

--- src/handlers/processAuctions.js
+++ src/handlers/processAuctions.js
@@ -1,5 +1,9 @@
+import { getEndedAuctions } from '../lib/getEndedAuctions';
+
 async function processAuctions(event, context) {
-    console.log('Processing Auctions');
+    const auctionsToClose = await getEndedAuctions();
+    
+    console.log(auctionsToClose);
 }
 
 export const handler = processAuctions;
