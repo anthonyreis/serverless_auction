c2ee2c94998c6739b08b2cc4dfe8aae9234a13f1
anthonyreis <matheus.anthony1@gmail.com>
2022-01-27 09:05:30 -0300

    adicionando recurso do sqs

--- src/handlers/placeBid.js
+++ src/handlers/placeBid.js
@@ -7,12 +7,21 @@ import placeBidSchema from '../lib/schemas/placeBidSchema';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-async function getAuction(event, context) {
+async function placeBid(event, context) {
     const { id } = event.pathParameters;
     const { amount } = event.body;
+    const { email } = event.requestContext.authorizer;
     let updatedAuction;
 
     const auction = await getAuctionById(id);
+
+    if (auction.highestBid.bidder === email) {
+        throw new createError.Forbidden(`You cannot bid, because you are already the highest bidder!`);
+    }
+
+    if (auction.seller === email) {
+        throw new createError.Forbidden(`You cannot bid on your own auctions!`);
+    }
 
     if (auction.status !== 'OPEN') {
         throw new createError.Forbidden(`You cannot bid on closed auctions!`)
@@ -25,9 +34,10 @@ async function getAuction(event, context) {
     const params = {
         TableName: process.env.AUCTIONS_TABLE_NAME,
         Key: { id },
-        UpdateExpression: 'set highestBid.amount = :amount',
+        UpdateExpression: 'set highestBid.amount = :amount, highestBid.bidder = :bidder',
         ExpressionAttributeValues: {
             ':amount': amount,
+            ':bidder': email
         },
         ReturnValues: 'ALL_NEW',
     }
@@ -48,5 +58,5 @@ async function getAuction(event, context) {
     };
 }
 
-export const handler = commonMiddleware(getAuction)
+export const handler = commonMiddleware(placeBid)
     .use(validator({ inputSchema: placeBidSchema }))
